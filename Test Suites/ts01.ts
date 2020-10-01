8c4d85d3b8cf3a4b7d2076fd151ad2305289685d
hoaiduc <paypal.dhtran789@gmail.com>
2020-10-01 11:15:36 +0700

    test-file

---
+++ Test Suites/ts01.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ts01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4edb18fd-2d10-45a4-ace7-9e920cb513b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>58f5abbf-00eb-41ab-9f87-1a02f2c59549</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc01</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>9d8120b9-8d44-4a22-9966-b205c4598184</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3e8306f1-a725-4448-9547-746515b2bebc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc02</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c269c0e5-acba-4a57-876b-548d564d68d0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>21142d6f-02c8-4d9f-b15d-e5e407d7ba33</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc03</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>048bdf10-7dfe-479f-b7ee-8bf14a658bf3</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>78b62734-9ca7-4529-a87d-c56fd92d21ef</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc04</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e0e0fecf-08af-44a0-9c2f-5007c98ab650</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/tc05</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>450e317e-8e37-4124-8ba5-4ab3de29a1b6</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
